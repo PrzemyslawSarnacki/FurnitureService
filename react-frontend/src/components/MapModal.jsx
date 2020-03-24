5ce1f7e3db9b17b47ae851a18b392eaabfc94e8d
PrzemyslawSarnacki <przemos100@gmail.com>
2020-03-24 16:53:38 +0100

    Location and Fullscreen

--- react-frontend/src/components/MapModal.jsx
+++ react-frontend/src/components/MapModal.jsx
@@ -17,10 +17,12 @@ class MapModal extends React.Component {
           onCancel={this.props.handleCancel}
           footer={[
             <Button key="back" onClick={this.props.handleCancel}>
-              Return
+              Wróć
             </Button>,
-            <Button key="submit" type="primary" loading={this.props.loading} onClick={this.props.handleOk}>
-              Submit
+            <Button key="submit" type="danger" loading={this.props.loading} onClick={this.props.handleOk}>
+              <a href="https://www.google.com/maps/place/Meble+idea/@53.1639094,22.0544312,15z/data=!4m5!3m4!1s0x0:0x85616f733b3c1f81!8m2!3d53.1639094!4d22.0544312">
+                Pełny ekran
+              </a>
             </Button>,
           ]}
         >
@@ -28,6 +30,11 @@ class MapModal extends React.Component {
           <div style={{ width: "auto", height: 400 }}>
             <Map google={this.props.google} zoom={14}
               style={{ width: 400, height: 400 }}
+              initialCenter={{
+                lat: 53.1639094,
+                lng: 22.0544312,
+              }}
+    
             >
               <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
