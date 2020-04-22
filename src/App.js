826c0d5c18ba0762f2c769f4584dffbf48dfabbf
PrzemyslawSarnacki <przemos100@gmail.com>
2020-04-23 00:21:57 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router basename='/FurnitureService'>
           <CustomLayout {...this.props}>
           <ScrollToTop>
               <BaseRouter />
